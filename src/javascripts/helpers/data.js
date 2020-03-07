846320e9c44298bf10e8b779bba7ec430ca40ce3
Bethany Whitworth <bethanyjwhitworth@gmail.com>
2020-03-07 09:02:19 -0600

    eat quad works

--- src/javascripts/helpers/data.js
+++ src/javascripts/helpers/data.js
@@ -11,10 +11,6 @@ const feedHealthyFood = () => {
   scorePrinter('eat-score-container', pet.full);
 };
 
-//These functions need to be in the eat.js component, not in Data. 
-//Data should only hold the object.
-//move this stuff back into eat.js and then make a function for Setting the new values
-
 const feedJunkFood = () => {
   if (pet.full >= 3) { pet.full -= 3; } else { pet.full = 0; }
   scorePrinter('eat-score-container', pet.full);
